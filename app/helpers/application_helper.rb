c4e10dd6ecb3dbd76f4d7abf2174b85b29c26268
Randy Harmon <rjharmon0316@yahoo.com>
2009-01-13 01:05:55 -0800

    Pulled boxes out into their own helper method.  Demonstrated use of boxes.
    Adjusted CSS so that class=box can be used, regardless of the context; left-boxes get styled differently than right-boxes, but both can use box() helper.
    
    Created helpers for menu items with shortcut keys in-theme
    
    Added flash areas for notification messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,40 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+	def box( *args, &block )
+		options = args.extract_options!
+
+		if @location then
+			 raise SiteDevError("@location is currently reserved for box() helper")
+		end
+		if @image then
+			raise SiteDevError("@image is currently reserved for box() helper")
+		end
+		@location = options[:location] || :right
+		@image = options[:image]
+		render :layout => "shared/box" do
+			yield
+		end
+		@location = nil
+		@image = nil
+	end
+
+	def shortcut_key( key, label )
+		label.sub!( /#{key}/, "<span class=\"shortcut\">#{h key}</span>" );
+		label
+	end
+
+	def menu_item( shortcut, label, options, *more )
+		t = "<li"
+		if( current_page?(options) ) then
+			t.concat ' class="active"'
+		end
+		t.concat '>'
+		t.concat link_to( shortcut_key( shortcut, label ), options, *more )
+		t.concat "</li>"
+		t
+	end
+end
+
+class SiteDevError < Exception
 end
