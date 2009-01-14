698add644a1aace9c511c83fc4e5065ec1007997
Randy Harmon <rjharmon0316@yahoo.com>
2009-01-14 12:19:02 -0800

    Added tests for menu_item helper.  All green

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,4 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+include ApplicationHelper
 
 describe ApplicationHelper do
   
@@ -9,14 +10,36 @@ describe ApplicationHelper do
   	it "should allow the content to be placed in arbitrary section names"
   end
   
-  describe "menu-item creation helper" do 
+  describe "menu_item" do
+
 	it "should return a menu item with a link"
 	it "should highlight the shortcut key so the user knows to use it"
 	describe "for the current item" do
-		it "should be tagged as the current item"
+		before :each do
+			should_receive(:current_page?).and_return( true )
+			@result = menu_item("a", "Apples", "/")
+		end
+		it "should have a link in there" do
+			@result.should have_tag("li>a")
+		end
+		it "should be tagged as the current item" do
+			@result.should have_tag("li.active", "Apples")
+		end
 	end
 	describe "for a non-current item" do
-		it "should not be tagged as the current item"
+		before :each do
+			should_receive(:current_page?).and_return( false )
+			@result = menu_item("a", "Apples", "/")
+		end
+		it "should show a link tag" do
+			@result.should have_tag("a", "Apples")
+		end
+		it "should include an li" do
+			@result.should have_tag("li")
+		end
+		it "should not be tagged as the current item" do
+			@result.should_not have_tag("li.active")
+		end
 	end
   end  
 end
