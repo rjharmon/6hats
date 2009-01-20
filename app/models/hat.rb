d5cc45a794177f01a23253c10805ba2f7cfc63ad
Randy Harmon <rjharmon0316@yahoo.com>
2009-01-20 10:48:36 -0800

    More docs

--- app/models/hat.rb
+++ app/models/hat.rb
@@ -1,2 +1,31 @@
 class Hat < ActiveRecord::Base
+# states available:
+# * Fresh (creating a new topic)
+#   * -> Explore Emotions (vent)
+#   * -> Define the Problem
+#   * User may also not be sure what the problem is -> Fact Finding
+# * Define the problem
+#   * -> Ideation (explore possibilities)
+#   * -> Potential and Motivation (imagine what could be possible)
+#   * -> Fact Finding (recognize truths/factual constraints)
+# * Fact Finding
+#   * -> Ideation (how can we use these facts)
+#   * Note: this may be like blue hat - push and pop this state as an in-built 
+#     part of the flow, instead of it being a step on a linear path
+# * Ideation
+#   * -> Potential & Motivation (what's good about this idea, where could it lead?)
+#   * -> Problem Identification (what problems could arise with these ideas?)
+#   * -> Explore Emotions (how do we feel about these ideas?)
+# * Potential and Motivation
+#   * -> Ideation (How could this potential actually be achieved?)
+#   * -> Problem Identification (What problems can we identify and possibly work around?)
+# * Problem Identification
+#   * -> Potential & Motivation (What reasons would we have for finding a solution to these problems?)
+#   * -> Ideation (What ideas do we have for solving these problems?)
+#   * -> Explore Emotions (How do we feel about these problem?)
+# * Explore Emotions
+#   * not actually an endpoint, but no explicit next actions 
+#   * use it prior to more objective steps (yellow/black) to increase objectivity
+# * Solve the problem
+#   * Recognize the thoughts already expressed which are the most salient to the objectives
 end
--- app/models/thought.rb
+++ app/models/thought.rb
@@ -1,4 +1,28 @@
 class Thought < ActiveRecord::Base
 	belongs_to :topic
 # TODO:	belongs_to :hat
+
+
+# Target thoughts to automatically create:
+# * Ideation
+#   * H -> Potential & Motivation (what's good about this idea, where could it lead?)
+#   * M -> Problem Identification (what problems could arise with these ideas?)
+#   * L -> Explore Emotions (how do we feel about these ideas?)
+# * Potential and Motivation
+#   * M -> Ideation (How could this potential actually be achieved?)
+#   * H -> Problem Identification (What problems can we identify and possibly work around?)
+#   * H-> Emotions (Intuition on which ones will be strong contenders)
+# * Problem Identification
+#   * H-> Potential & Motivation (What reasons would we have for finding a solution to these problems?)
+#   * M -> Ideation (What ideas do we have for solving these problems?)
+#   * L -> Explore Emotions (How do we feel about these problem?)
+# * Explore Emotions
+#   * not actually an endpoint, but no explicit next actions 
+#   * use it prior to more objective steps (yellow/black) to increase objectivity
+# * Solve the problem
+#   * Recognize the thoughts already expressed which are the most salient to the objectives
+
+
 end
+
+
