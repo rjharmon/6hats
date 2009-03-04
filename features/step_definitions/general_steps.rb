0d6fb381db5f16311abcf04238d565bf2a4d8e02
Randy Harmon <rjharmon0316@yahoo.com>
2009-03-04 10:45:13 -0800

    Steps that round out the email testing and fix up the login-as-pickle-generated-user

--- features/step_definitions/general_steps.rb
+++ features/step_definitions/general_steps.rb
@@ -1,21 +1,33 @@
 Given /I\'m logged in/ do   
 #'
-  Given 'a user: "me" exists with login: "myself"'
+#  debugger
+# Given 'a user: "me" exists with login: "myself"'
+  Given "an activated user named 'myself'"
+  Then 'user: "me" should exist with login: "myself"'
   @user = model 'user: "me"'
-  pp @user
-  activate_user!( @user.activation_code )
   Given 'I am logged in as user: "me"'
 end
 
 Given "I am logged in as #{capture_model}" do |user|
-  u = model(user)
-  log_in_user!( { :password => 'fake passwd', :login => u.login } )
+  log_in_user!( { :password => 'fake passwd', :login => @user.login } )
+end
+
+Given "I have no topics" do 
+  @user.topics.size.should == 0
 end
 
 Then /let me see the results/ do 
   save_and_open_page
 end
 
+Then /it should send (\d+) email[s]?/ do |count|
+    ActionMailer::Base.deliveries.length.should == count.to_i
+end
+
+Then /^\w+ should get an email with \w+ activation code$/ do
+  email = ActionMailer::Base.deliveries[0]
+  email.body.should =~ Regexp.new( @user.activation_code )
+end
 
  def activate_user activation_code=nil
    activation_code = @user.activation_code if activation_code.nil?
