44b46c86fedbf50fd98f9ce88109ac1d943b3962
Randy Harmon <rjharmon0316@yahoo.com>
2009-06-30 09:03:28 -0700

    Move thoughts routing to routing spec dir.

--- spec/routing/thoughts_routing_spec.rb
+++ spec/routing/thoughts_routing_spec.rb
@@ -2,24 +2,16 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe ThoughtsController do
   describe "route generation" do
-      it "should NOT map #index" do
-        route_for(:controller => "thoughts", :action => "index", :topic_id => "1").should == ""#  raise_error( ActionController::MethodNotAllowed )
-      end
-        it "should NOT map #indewfefwex" do
-          route_for(:controller => "thoughts", :action => "wefoijewfoij", :topic_id => "1").should == ""#  raise_error( ActionController::MethodNotAllowed )
-        end
+    it "should NOT map #index" do
+#      { :get => "/thoughts/" }.should_not be_routable
+    end
+    it "should NOT map #show" do
+ #     { :get => "/thoughts/1" }.should_not be_routable
+    end
   
     it "should map #new" do
       route_for(:controller => "thoughts", :action => "new", :topic_id => "1").should == "/topics/1/thoughts/new"
     end
-  
-    it "should NOT map #show" do
-      bypass_rescue
-      lambda do 
-        route_for(:controller => "thoughts", :action => "show", :id => "1", :topic_id => "1") 
-      end.should raise_error( ActionController::RoutingError )
-    end
-  
     it "should map #edit" do
       route_for(:controller => "thoughts", :action => "edit", :id => "1", :topic_id => "1").should == "/topics/1/thoughts/1/edit"
     end
@@ -34,20 +26,12 @@ describe ThoughtsController do
   end
 
   describe "route recognition" do
-    it "should NOT be able to get /topics/1/thoughts" do
-      lambda { params_from(:get, "/topics/1/thoughts")}.should raise_error( ActionController::MethodNotAllowed )
-    end
-  
     it "should generate params for #new" do
       params_from(:get, "/topics/1/thoughts/new").should == {:controller => "thoughts", :action => "new", :topic_id => "1"}
     end
   
     it "should generate params for #create" do
       params_from(:post, "/topics/1/thoughts").should == {:controller => "thoughts", :action => "create", :topic_id => "1"}
-    end
-  
-    it "should generate params for #show" do
-      lambda { params_from(:get, "/topics/1/thoughts/1") }.should raise_error( ActionController::MethodNotAllowed )
     end
   
     it "should generate params for #edit" do
